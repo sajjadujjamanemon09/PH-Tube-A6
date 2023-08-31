60568c46317f14ab3f36f10ba86bf0fb98724fbb
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2023-08-31 20:41:26 +0600

    create category section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log('object');
+const handleCategory = async() => {
+    const response = await fetch("https://openapi.programming-hero.com/api/videos/categories")
+    const data = await response.json()
+
+    const tabContainer = document.getElementById("tab-container")
+    data.data.forEach((category) => {
+        console.log(category);
+        const div = document.createElement("div")
+        div.innerHTML = `
+        <a class="tab bg-gray-200 rounded-sm">${category.category}</a> 
+        `
+
+        tabContainer.appendChild(div)
+    })
+
+    console.log(data.data);
+}
+
+
+
+
+
+
+
+
+
+
+handleCategory()
